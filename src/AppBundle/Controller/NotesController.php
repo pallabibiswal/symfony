277fb29c73a04bfcd42876e59724d41415a6574c
pallabi <pallabi.biswal@mindfiresolutions.com>
2018-06-26 23:04:27 +0530

    initial commit

--- src/AppBundle/Controller/NotesController.php
+++ src/AppBundle/Controller/NotesController.php
@@ -5,20 +5,24 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\SerializerInterface;
+
 
 class NotesController extends Controller
 {
     /**
      * @Route("/", name = "home_page")
      */
-    public function indexAction(SerializerInterface $serializer)
+    public function indexAction()
     {
-//        return $this->render("Notes/index.html.twig",[
-//            "name" => "Pallabi"
-//        ]);
+        return $this->render("Notes/index.html.twig");
+    }
 
-        $data = $serializer->serialize(array("name" => "pallabi"), 'json');
-        return new Response($data);
+    /**
+     * @Route("/data", name="save_data")
+     */
+    public function SaveData()
+    {
+        print_r($_POST);exit;
+//        return new Response("pallabi");
     }
 }
---
+++ src/AppBundle/Entity/Vote.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Vote
+ *
+ * @ORM\Table(name="vote")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\VoteRepository")
+ */
+class Vote
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="file", type="string", length=255)
+     */
+    private $file;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Vote
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Vote
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set file
+     *
+     * @param string $file
+     *
+     * @return Vote
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * Get file
+     *
+     * @return string
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+}
+
